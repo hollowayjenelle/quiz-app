dd4cc7a0393727bbf6b936ccaae0c7a3c85dde8b
jholloway <jholloway@griddynamics.com>
2023-02-08 09:56:12 -0500

    add start quix card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import {useState} from 'react';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
+  const [startQuiz, setStartQuiz] = useState<boolean>(false)
+
+  const toggle = () => {
+    setStartQuiz(prevVal => !prevVal)
+  }
   return (
     <div className="App">
-      <Card/>
+     {startQuiz ?
+     <Card/> :
+     <div className='start-card'>
+        <h2>Welcome to the Music Quiz</h2>
+        <h4>Click the button below to start</h4>
+        <button onClick={toggle}>BEGIN</button>
+      </div>}
     </div>
   );
 }
