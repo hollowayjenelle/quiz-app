d94b6fd8b55fe4254c86277f9a049382b69d4d51
jholloway <jholloway@griddynamics.com>
2023-02-10 11:33:51 -0500

    prettier enabled

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,97 +1,107 @@
 /**
  * TODO
  * Add CSS
- * 
+ *
  */
-import React, {FC, useState, useEffect, useMemo} from 'react';
-import {Question} from './interfaces'
-import AnswerButtons from './AnswerButtons';
+import React, { FC, useState, useEffect, useMemo } from "react";
+import { Question } from "./interfaces";
+import AnswerButtons from "./AnswerButtons";
 
-const Card : FC = () => {
-    const [questions, setQuestions] = useState<Question[]>([])
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0)
-    const [score, setScore] = useState<number>(0)
-    const [currentAnswer, setCurrentAnswer] = useState<string>('')
-    const [showScore, setShowScore] = useState<boolean>(false)
+const Card: FC = () => {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [currentAnswer, setCurrentAnswer] = useState<string>("");
+  const [showScore, setShowScore] = useState<boolean>(false);
 
+  const incorrectAnswers = questions[currentQuestionIndex]?.incorrectAnswers;
+  const correctAnswer = questions[currentQuestionIndex]?.correctAnswer;
 
-    const incorrectAnswers = questions[currentQuestionIndex]?.incorrectAnswers
-    const correctAnswer = questions[currentQuestionIndex]?.correctAnswer
+  const allAnswers = incorrectAnswers?.concat(correctAnswer);
 
-    const allAnswers = incorrectAnswers?.concat(correctAnswer)
-    
+  useEffect(() => {
+    fetch(
+      "https://the-trivia-api.com/api/questions?categories=music&limit=10&difficulty=easy"
+    )
+      .then((response) => response.json())
+      .then((res) => setQuestions(res));
+    console.log("Questions loaded");
+  }, []);
 
-    useEffect(() =>{
-        fetch('https://the-trivia-api.com/api/questions?categories=music&limit=10&difficulty=easy')
-        .then(response => response.json())
-        .then(res => setQuestions(res))
-        console.log('Questions loaded')
-    }, [])
+  const shuffle = (arr: string[]) => {
+    arr?.sort(() => 0.5 - Math.random());
+    return arr;
+  };
 
-    const shuffle = (arr: string[]) => {
-        arr?.sort(() => 0.5 - Math.random())
-        return arr
-    }
+  // eslint-disable-next-line
+  const shuffledAnswers = useMemo(
+    () => shuffle(allAnswers),
+    [questions[currentQuestionIndex]]
+  );
 
-    // eslint-disable-next-line
-    const shuffledAnswers = useMemo(() => shuffle(allAnswers), [questions[currentQuestionIndex]])
-
-    const answersBtns = shuffledAnswers?.map( ans => {
-        return <AnswerButtons 
-        key={ans} 
-        answer={ans} 
-        answerState={currentAnswer}
-        changeFunc={getAnswer}/>
-    })
-
-    function assignPoint(){
-        if(currentAnswer === questions[currentQuestionIndex].correctAnswer){
-            console.log('Score increased')
-            setScore(prevScore => prevScore + 1)
-        }
-    }
-
-    function changeQuestion(){
-        assignPoint()
-        if(currentQuestionIndex < questions.length){
-            setCurrentQuestionIndex(prevCount => prevCount + 1)
-            resetAnswer()
-        }
-    }
-
-    function displayScore(){
-        assignPoint()
-        setShowScore(prevVal => !prevVal)
-    }
-
-    function getAnswer(event: React.SyntheticEvent){
-        const target = event.target as HTMLInputElement
-        setCurrentAnswer(prevValue => target.value)
-    }
-
-    function resetAnswer(){
-        setCurrentAnswer('')
-    }
-
+  const answersBtns = shuffledAnswers?.map((ans) => {
     return (
-        <div className='card-section'>
-            {(showScore) ? 
-                <div className='score-card'>
-                    <h2>Your score is {score}</h2>
-                    <h3>{score}</h3>
-                </div> 
-            :   
-                <div className='quiz-card'>
-                    <h3>Question {currentQuestionIndex + 1} out of {questions.length}</h3>
-                    <p>{questions[currentQuestionIndex]?.question}</p>
-                    <div>
-                        {answersBtns}
-                    </div>
-                    {currentQuestionIndex < questions.length-1 ? <button onClick={changeQuestion}>Next Question</button> : <button onClick={displayScore}>Show Score</button>}
-                </div>
-            }
-        </div>
+      <AnswerButtons
+        key={ans}
+        answer={ans}
+        answerState={currentAnswer}
+        changeFunc={getAnswer}
+      />
     );
+  });
+
+  function assignPoint() {
+    if (currentAnswer === questions[currentQuestionIndex].correctAnswer) {
+      console.log("Score increased");
+      setScore((prevScore) => prevScore + 1);
+    }
+  }
+
+  function changeQuestion() {
+    assignPoint();
+    if (currentQuestionIndex < questions.length) {
+      setCurrentQuestionIndex((prevCount) => prevCount + 1);
+      resetAnswer();
+    }
+  }
+
+  function displayScore() {
+    assignPoint();
+    setShowScore((prevVal) => !prevVal);
+  }
+
+  function getAnswer(event: React.SyntheticEvent) {
+    const target = event.target as HTMLInputElement;
+    setCurrentAnswer((prevValue) => target.value);
+  }
+
+  function resetAnswer() {
+    setCurrentAnswer("");
+  }
+
+  return (
+    <div className="card-section">
+      {showScore ? (
+        <div className="score-card">
+          <h2>Your score is {score}</h2>
+          <h3>{score}</h3>
+        </div>
+      ) : (
+        <div className="quiz-card">
+          <h3>
+            Question {currentQuestionIndex + 1} out of {questions.length}
+          </h3>
+          <p>{questions[currentQuestionIndex]?.question}</p>
+          <div>{answersBtns}</div>
+          {currentQuestionIndex < questions.length - 1 ? (
+            <button onClick={changeQuestion}>Next Question</button>
+          ) : (
+            <button onClick={displayScore}>Show Score</button>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Card;
